0fb70b0733217ee658453f82cc1cc429591c0f5a
Burlak Ilia <burlakilia@bk.ru>
2018-07-19 14:57:10 +0300

    add users stub

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const cors = require('@koa/cors');
 
 const app = new Koa();
 const router = new Router();
@@ -15,8 +16,8 @@ router.get('/users', (ctx, next) => {
     ctx.body = JSON.stringify([{name: 'ilia'}]);
 });
 
-
 app
+    .use(cors())
     .use(router.routes())
     .use(router.allowedMethods())
     .use(async ctx => {
