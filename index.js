51ab3103f58094c4bf4552be0c60f2132590604b
Burlak Ilia <burlakilia@bk.ru>
2018-07-19 14:35:26 +0300

    add users stub

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
+const app = new Koa();
+const router = new Router();
+
+// POST /signup
+// POST /sigin
+// GET /messages
+// GET /users
+// WSS /chat
+
+
+router.get('/users', (ctx, next) => {
+    ctx.body = JSON.stringify([{name: 'ilia'}]);
 });
+
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .use(async ctx => {
+        ctx.body = 'Use api methods please';
+    });
 
 console.log('started at', process.env);
 app.listen(process.env.PORT || 3000);
